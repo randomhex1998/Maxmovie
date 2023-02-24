1fe299925041699d992c81c875eb8d7d88c7b083
rayan <randomhex1998@gmail.com>
2023-02-24 16:13:25 +0330

    add fontawesome to project and add ico to btns

--- src/shared/Button.jsx
+++ src/shared/Button.jsx
@@ -1,6 +1,7 @@
-const Button = ({ btnText, btnId }) => {
+const Button = ({ btnText, btnId = "#", btnIco = "#" }) => {
   return (
     <a href="#" className="btn" id={btnId}>
+      <i className={btnIco}></i>
       {btnText}
     </a>
   );
