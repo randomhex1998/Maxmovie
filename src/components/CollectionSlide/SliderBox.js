47ddf85be49776064cf4cf44d5477c29c581d4c7
rayan <randomhex1998@gmail.com>
2023-03-10 08:47:02 +0330

    add movies poster and add autoplay to coll slider

--- src/components/CollectionSlide/SliderBox.js
+++ src/components/CollectionSlide/SliderBox.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const SliderBox = ({collIMG , collTxt , }) => {
     return (
-        <div className='coll-box'>
+        <a href='#' className='coll-box'>
             <div className='coll-img-area'>
                 <img src={collIMG} className="coll-img"></img>
             </div>
             <p className='coll-text'>{collTxt}</p>
-        </div>
+        </a>
     );
 };
 
--- src/components/CollectionSlide/SliderSwiper.js
+++ src/components/CollectionSlide/SliderSwiper.js
@@ -7,17 +7,29 @@ import SliderBox from "./SliderBox";
 import "swiper/css";
 
 // import required modules
-import { Pagination } from "swiper";
+import { Pagination , Autoplay } from "swiper";
 
 //Import Local Images
 import Harry from "../../img/movies/HarryPotter.jpg";
+import Fast from "../../img/movies/Fast.jpg";
+import Mission from "../../img/movies/Mission.jpg";
+import MenInBlack from "../../img/movies/MenInBlack.jpg";
+import Habit from "../../img/movies/Habit.jpg";
+import Avatar from "../../img/movies/AVT.jpeg";
+import KingsMan from "../../img/movies/KingsMan.jpg";
+import Maze from "../../img/movies/Maze.jpg";
 
 const SliderSwiper = () => {
   return (
     <div>
       <Swiper
+        loop={true}
         slidesPerView={1}
         spaceBetween={10}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
         breakpoints={{
           "@0.00": {
             slidesPerView: 1,
@@ -36,31 +48,31 @@ const SliderSwiper = () => {
             spaceBetween: 50,
           },
         }}
-        modules={[Pagination]}
+        modules={[Autoplay]}
         className="mySwiper">
         <SwiperSlide>
           <SliderBox collIMG={Harry} collTxt="هری پاتر" />
         </SwiperSlide>
         <SwiperSlide>
-          <SliderBox collIMG={Harry} collTxt="هری پاتر" />
+          <SliderBox collIMG={Fast} collTxt="سریع و خشن " />
         </SwiperSlide>
         <SwiperSlide>
-          <SliderBox collIMG={Harry} collTxt="هری پاتر" />
+          <SliderBox collIMG={Mission} collTxt="ماموریت غیر ممکن" />
         </SwiperSlide>
         <SwiperSlide>
-          <SliderBox collIMG={Harry} collTxt="هری پاتر" />
+          <SliderBox collIMG={MenInBlack} collTxt="مردان سیاه پوش" />
         </SwiperSlide>
         <SwiperSlide>
-          <SliderBox collIMG={Harry} collTxt="هری پاتر" />
+          <SliderBox collIMG={Habit} collTxt="ارباب حلقه ها" />
         </SwiperSlide>
         <SwiperSlide>
-          <SliderBox collIMG={Harry} collTxt="هری پاتر" />
+          <SliderBox collIMG={Avatar} collTxt="آواتار" />
         </SwiperSlide>
         <SwiperSlide>
-          <SliderBox collIMG={Harry} collTxt="هری پاتر" />
+          <SliderBox collIMG={KingsMan} collTxt="کینگز من" />
         </SwiperSlide>
         <SwiperSlide>
-          <SliderBox collIMG={Harry} collTxt="هری پاتر" />
+          <SliderBox collIMG={Maze} collTxt="دونده هزارتو" />
         </SwiperSlide>
       </Swiper>
     </div>
